8dc6b01ff9ec4eb6bf42bd55a263fbc9ffea056d
HAP <hproctor@redhat.com>
2021-01-05 12:46:00 -0500

    Fix local tagging

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -5,8 +5,9 @@ PRESTO_VER=$(${SCRIPT_DIR}/get_presto_version.sh)
 
 echo "Executing local presto docker image build..."
 docker build \
-       -t local-koku-presto:latest \
-       -t local-koku-presto:${PRESTO_VER} \
+       -t quay.io/cloudservices/ubi-presto:latest \
+       -t quay.io/cloudservices/ubi-presto:${PRESTO_VER} \
        -f "${SCRIPT_DIR}/Dockerfile" \
        $@ \
        "${SCRIPT_DIR}"
+
